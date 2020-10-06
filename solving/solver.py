01ec8d957257aa5f295501f61285a12cdc99d170
Soliman006 <63769601+Soliman006@users.noreply.github.com>
2020-10-07 03:05:21 +1100

    Update solver.py

--- solving/solver.py
+++ solving/solver.py
@@ -110,7 +110,7 @@ class SolverData:
         
     def fullsolver_columns(self):
         self.solveborder2()
-        self.settings.helper_threshold = 1.03
+        #self.settings.helper_threshold = 1.03
         for i in range(1,self.data.puzzle_columns-1):
             self.hardReset()
             self.placecurrentsolution()
