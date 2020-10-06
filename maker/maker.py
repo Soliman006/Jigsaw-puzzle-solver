4689146dc74199d6e9a1c29e1ff49d6e777dbbfc
Soliman006 <63769601+Soliman006@users.noreply.github.com>
2020-10-06 19:30:25 +1100

    Update maker.py

--- maker/maker.py
+++ maker/maker.py
@@ -15,6 +15,7 @@ class PuzzleMaker:
         """Initialisation."""
         self.img_input = img
         self.img_input_h, self.img_input_w, self.img_input_ch = self.img_input.shape
+        print(self.img_input_ch)
         self.img_input_blank = np.zeros([self.img_input_h, self.img_input_w, self.img_input_ch], dtype=np.uint8)
         self.n_rows = n_rows
         self.settings = settings
