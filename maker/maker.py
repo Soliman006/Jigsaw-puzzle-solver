7c48d7583141bd607b962c7d773b3f6b3ab459e5
Soliman006 <63769601+Soliman006@users.noreply.github.com>
2020-10-06 19:47:10 +1100

    Update maker.py

--- maker/maker.py
+++ maker/maker.py
@@ -16,7 +16,9 @@ class PuzzleMaker:
         self.img_input = img
         self.img_input_h, self.img_input_w, self.img_input_ch = self.img_input.shape
         print(self.img_input_h, self.img_input_w, self.img_input_ch)
-        self.img_input_blank = np.zeros([self.img_input_h, self.img_input_w, self.img_input_ch], dtype=np.uint8)
+        self.background_colour = np.array([0, 255, 0])
+        self.img_input_blank = np.fill([self.img_input_h, self.img_input_w, self.background_colour], dtype=np.uint8)
+        #self.img_input_blank = np.zeros([self.img_input_h, self.img_input_w, self.img_input_ch], dtype=np.uint8)
         print(self.img_input_blank)
         self.n_rows = n_rows
         self.settings = settings
