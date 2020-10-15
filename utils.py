2282aac4f29e9117b3f719586f0bf5ef4a7539ab
Soliman006 <63769601+Soliman006@users.noreply.github.com>
2020-10-15 12:03:00 +1100

    Update utils.py

--- utils.py
+++ utils.py
@@ -183,15 +183,15 @@ def retrieveExample(i):
         settings.helper_threshold = 1.3 # all options within this multiplier are considered
         settings.max_options = 2'''
         
-        elif i == 24:
-            settings.bg_thresh_low = hsv_to_cvhsv(85, 30, 30)
-            settings.bg_thresh_high = hsv_to_cvhsv(140, 100, 100)
-            settings.e_contour_smoothing = 0.5
-            settings.score_thresh = 1.9  # if no options are under this then it trigger the backtracker
-            settings.helper_threshold = 1.3  # all options within this multiplier are considered
-            settings.max_options = 2
-            settings.interpolate_ref = False
-            settings.interpolate_cand = False
+    elif i == 24:
+        settings.bg_thresh_low = hsv_to_cvhsv(85, 30, 30)
+        settings.bg_thresh_high = hsv_to_cvhsv(140, 100, 100)
+        settings.e_contour_smoothing = 0.5
+        settings.score_thresh = 1.9  # if no options are under this then it trigger the backtracker
+        settings.helper_threshold = 1.3  # all options within this multiplier are considered
+        settings.max_options = 2
+        settings.interpolate_ref = False
+        settings.interpolate_cand = False
         
     return img, settings
 
